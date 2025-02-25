886b6d8cf010643f51c34ba14879c5c989b60e57
Sasi <sasikiran.tt3011@gmail.com>
2025-02-25 12:02:57 +0530

    Added

---
+++ Arithemitic operator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<"a+b"<<a+b<<endl;
+    cout<<"a-b"<<a-b<<endl;
+    cout<<"a*b"<<a*b<<endl;
+    cout<<"a/b"<<a/b<<endl;
+    cout<<"Post increment = a++"<<a++<<endl;
+    cout<<"Pre increment = ++a"<<++a<<endl;
+    cout<<"Post decrement = b--"<<b--<<endl;
+    cout<<"Pre decrement = --b"<<--b<<endl;
+    return 0;
+}
